e0b17d0f03a938e5ecc62498b6219aadadc02628
Graziely Tds <119763827+GrazielyTavares@users.noreply.github.com>
2023-11-22 16:04:32 -0300

    Create AppLaucher.java

---
+++ scr1/src/AppLaucher.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class AppLaucher{
+
+    public static void main(String[] args){
+
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+
+                // display our weather app gui
+                new WeatherAppGui().setVisible(true);
+            }
+
+        });
+    }
+}
